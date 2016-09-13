718509285c4c485e53cc83ea9d12eb8c77cb11b8
Matthew Reilly <matt@matthews-air.dhcp.nd.edu>
2016-09-13 00:47:51 -0400

    RTT

--- UDPClient/udpclient.c
+++ UDPClient/udpclient.c
@@ -10,7 +10,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
+#include <time.h>
 
 #define BUFSIZE 4096
 
@@ -27,11 +28,12 @@ int main(int argc, char **argv) {
     struct hostent *server;
     char *hostname;
     char buf[BUFSIZE];
+    clock_t start, stop;
 
     /* check command line arguments */
     // CHANGE <text or file name>
-    if (argc != 3) {
-       fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
+    if (argc != 4) {
+       fprintf(stderr,"usage: %s <hostname> <port> <text or file name>\n", argv[0]);
        exit(0);
     }
 
@@ -59,19 +61,26 @@ int main(int argc, char **argv) {
 
     /* get a message from the user */
     bzero(buf, BUFSIZE);
-    printf("Please enter msg: ");
-    fgets(buf, BUFSIZE, stdin);
+    //printf("Please enter msg: ");
+    //fgets(buf, BUFSIZE, stdin);
+    buf = argv[3];
 
     /* send the message to the server */
     serverlen = sizeof(serveraddr);
     n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
+    start = clock();
     if (n < 0) 
       error("ERROR in sendto");
     
     /* print the server's reply */
     n = recvfrom(sockfd, buf, strlen(buf), 0, &serveraddr, &serverlen);
+    stop = clock();
     if (n < 0) 
       error("ERROR in recvfrom");
-    printf("Echo from server: %s", buf);
+    printf("Echo from server: %s\n", buf);
+
+    printf("%6.3f", start);
+    printf("\n\n%6.3f", stop);
+    
     return 0;
 }
